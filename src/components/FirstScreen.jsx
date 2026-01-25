f97a2842b893f3fff1c64ccdea412604cda35f4f
Luffy-maker60 <monekydluffy878@gmail.com>
2026-01-25 17:40:41 +0530

    Update FirstScreen.jsx

--- src/components/FirstScreen.jsx
+++ src/components/FirstScreen.jsx
@@ -59,7 +59,7 @@ export default function FirstScreen({ onNext }) {
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ delay: 1.5, duration: 0.8 }}
                 >
-                    Something that will change everything✨
+                    Something that will change everything if you agree✨
                 </motion.p>
 
                 <motion.div
